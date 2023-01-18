2e4b0dd3eb295f352296d38a671807153c85feda
ashagiro <aydanashagirova@gmail.com>
2023-01-18 19:55:39 +0600

    learn

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # groupie-tracker-trial
 
+Useful links :
+
+- https://tutorialedge.net/golang/parsing-json-with-golang/
+- https://www.w3schools.com/js/js_json_html.asp
--- main.go
+++ main.go
@@ -3,31 +3,84 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
-func main() {
-	jsonData := `
-		{
-			"intValue":1234,
-			"boolValue":true,
-			"stringValue":"hello!",
-			"dateValue":"2022-03-02T09:10:00Z",
-			"objectValue":{
-				"arrayValue":[1,2,3,4]
-			},
-			"nullStringValue":null,
-			"nullIntValue":null
-		}
-	`
-
-	var data map[string]interface{}
-	err := json.Unmarshal([]byte(jsonData), &data)
-	if err != nil {
-		fmt.Printf("could not unmarshal json: %s\n", err)
-		return
-	}
-
-	fmt.Printf("json map: %v\n", data)
+type Artists []struct {
+	// ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
-// https://blog.logrocket.com/using-json-go-guide/
+type Locations struct {
+	Index []struct {
+		Location []string `json:"locations"`
+	} `json:"index"`
+}
+
+func main() {
+	res0, err0 := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err0 != nil {
+		fmt.Print(err0.Error())
+		os.Exit(1)
+		// log.Fatal(err)
+	}
+	// defer res.Body.Close()
+
+	res1, err1 := http.Get("https://groupietrackers.herokuapp.com/api/locations")
+	if err1 != nil {
+		fmt.Print(err1.Error())
+		os.Exit(1)
+	}
+
+	text0, err := ioutil.ReadAll(res0.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text1, err := ioutil.ReadAll(res1.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var artists Artists
+	json.Unmarshal(text0, &artists)
+
+	var location Locations
+	json.Unmarshal(text1, &location)
+
+	// for i, p := range artists {
+	// 	fmt.Println("Name", (i + 1), ":", p.Name)
+	// 	fmt.Println("---------------------------")
+	// 	fmt.Println("Image", p.Image)
+	// 	fmt.Println("Members", p.Members)
+	// 	fmt.Println()
+	// }
+	// fmt.Println(location.Index)
+	for _, p := range location.Index {
+		fmt.Println("Locations", ":", p.Location)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := template.ParseFiles("templates/home.html")
+		if err != nil {
+			return
+		}
+		// t.Execute(w, "hello")
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Link -->   " + "http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
